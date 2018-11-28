68f03708087543798dc044e80e88c58e81c26ce2
yourvps <45386512+yourvps@users.noreply.github.com>
2018-11-28 09:40:17 +0800

    Update shadowsocks.sh

--- shadowsocks.sh
+++ shadowsocks.sh
@@ -154,7 +154,7 @@ pre_install(){
     # Set shadowsocks config password
     echo "请输入密码："
     read -p "(默认密码: haha.com):" shadowsockspwd
-    # shadowsockspwd='yourvpshaha'
+    read -p shadowsockspwd
     [ -z "${shadowsockspwd}" ] && shadowsockspwd="haha.com"
     echo
     echo "---------------------------"
